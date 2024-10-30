1d9e13ec0fdb947976e981f7d0db93fbaeaa075d
jh-lee99 <jun99h1219@gmail.com>
2024-10-30 21:55:27 +0900

    update 3-tier

--- mzc-backend/ormconfig.js
+++ mzc-backend/ormconfig.js
@@ -1,73 +1,39 @@
 const SnakeNamingStrategy = require("typeorm-naming-strategies").SnakeNamingStrategy
+const isTS = require('detect-ts-node');
 
 module.exports = {
-  "type": "mysql",
-  "host": process.env.DATABASE_HOST,
-  "port": process.env.DATABASE_PORT,
-  "username": process.env.DATABASE_USER,
-  "password": process.env.DATABASE_PASSWORD,
-  "database": process.env.DATABASE_DBNAME,
-  "synchronize": true,
-  "logging": false,
-  "entities": [
-    ".entity/**/*.js"
-  ],
-  "migrations": [
-    ".migration/**/*.js"
-  ],
-  "subscribers": [
-    ".subscriber/**/*.js"
-  ],
-  namingStrategy: new SnakeNamingStrategy()
-}
-
-// const SnakeNamingStrategy = require("typeorm-naming-strategies").SnakeNamingStrategy;
-// const isTS = require('detect-ts-node');
-
-// const config = {
-//   "name": "default",
-//   "type": "mysql",
-//   "replication": {
-//     "master": {
-//       "host": "192.168.30.101",
-//       "port": 3306,
-//       "username": "boarduser",
-//       "password": "boardpass",
-//       "database": "boarddb"
-//     },
-//     "slaves": [{
-//       "host": "192.168.30.101",
-//       "port": 3307,
-//       "username": "boarduser",
-//       "password": "boardpass",
-//       "database": "boarddb"
-//     }]
-//   },
-//   "synchronize": false,
-//   "logging": false,
-//   "namingStrategy": new SnakeNamingStrategy()
-// };
-
-// if (isTS) {
-//   config.entities = [
-//     "src/entity/**/*.ts"
-//   ];
-//   config.migrations = [
-//     "src/migration/**/*.ts"
-//   ];
-//   config.subscribers = [
-//     "src/subscriber/**/*.ts"
-//   ];
-// } else {
-//   config.entities = [
-//     "./entity/**/*.js"
-//   ];
-//   config.migrations = [
-//     "./migration/**/*.js"
-//   ];
-//   config.subscribers = [
-//     "./subscriber/**/*.js"
-//   ];
-// }
-
-// module.exports = config;
+  name: "default",
+  type: "mysql",
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  host: "localhost",
+  port: "3306",
+  username: "boarduser",
+  password: "boardpass",
+  database: "boarddb",
+  // replication: {
+  //   master: {
+  //     host: "192.168.10.101",
+  //     port: 3306,
+  //     username: "boarduser",
+  //     password: "boardpass",
+  //     database: "boarddb"
+  //   },
+  //   slaves: [{
+  //     host: "192.168.10.101",
+  //     port: 3307,
+  //     username: "boarduser",
+  //     password: "boardpass",
+  //     database: "boarddb"
+  //   },]
+  // },
+  synchronize: false,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: isTS ? ["src/entity/**/*.ts"] : ["dist/entity/**/*.js"],
+  migrations: isTS ? ["src/migration/**/*.ts"] : ["dist/migration/**/*.js"],
+  subscribers: isTS ? ["src/subscriber/**/*.ts"] : ["dist/subscriber/**/*.js"],
+};

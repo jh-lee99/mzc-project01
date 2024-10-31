16bd770dfb5296aa4c9a404b0c625d3831f8dfab
jh-lee99 <113870771+jh-lee99@users.noreply.github.com>
2024-10-31 16:21:16 +0900

    Update ormconfig.js

--- mzc-backend/ormconfig.js
+++ mzc-backend/ormconfig.js
@@ -9,6 +9,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  sid: process.env.DB_SID,
   // host: "localhost",
   // port: "3306",
   // username: "boarduser",
